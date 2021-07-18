8a989964956ede78cd1cea955db6ce86fca2900c
r616on <r616on@gmail.com>
2021-07-18 13:28:12 +0300

    puppitter_Done

--- src/e2e/credit-cart-validiti.test.js
+++ src/e2e/credit-cart-validiti.test.js
@@ -1,15 +1,15 @@
 import puppetteer from "puppeteer";
 
 jest.setTimeout(30000); // default puppeteer timeout
-describe("INN/OGRN form", () => {
+describe("cart form", () => {
   let browser = null;
   let page = null;
   const baseUrl = "http://localhost:9000";
   beforeAll(async () => {
     browser = await puppetteer.launch({
-      headless: false, // show gui
-      slowMo: 100,
-      devtools: true, // show devTools
+      headless: true, // show gui
+      slowMo: 0,
+      devtools: false, // show devTools
     });
     page = await browser.newPage();
   });
@@ -23,9 +23,8 @@ describe("INN/OGRN form", () => {
       await page.goto(baseUrl);
       const widget = await page.$(".credit-cart-validiti");
       const input = await widget.$(".input-row__input");
-      const buttom = await widget.$("input-row__button");
       await input.type("5480158084261928");
-      await buttom.waitForSelector("valid");
+      await page.waitForSelector(".input-row__button.valid");
     });
   });
 });
